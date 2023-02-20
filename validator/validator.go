756922dd02bebc7de11aa1466d9ec1c0a0f44634
liumurong1 <liumurong1@100tal.com>
2023-02-20 07:11:49 +0000

    修复xss

--- validator/validator.go
+++ validator/validator.go
@@ -859,7 +859,7 @@ func RemoveEmoji() Validator {
 
 // XSS 过滤XSS内容
 func XSS() Validator {
-	s1 := "(?i)<(\\/?)(script|iframe|style|html|body|title|link|meta|object|\\?|\\%)([^>]*?)>"
+	s1 := "(?i)<(\\/?)(a|abbr|acronym|address|applet|area|article|aside|audio|b|base|basefont|bdi|bdo|big|blockquote|body|br|button|canvas|caption|center|cite|code|col|colgroup|command|datalist|dd|del|details|dir|div|dfn|dialog|dl|dt|em|embed|fieldset|figcaption|figure|font|footer|form|frame|frameset|h1> to <h6|head|header|hr|html|i|iframe|img|input|ins|isindex|kbd|keygen|label|legend|li|link|map|mark|menu|meta|meter|nav|noframes|noscript|object|ol|optgroup|option|output|p|param|pre|progress|q|rp|rt|ruby|s|samp|script|section|select|small|source|span|strike|strong|style|sub|summary|sup|table|tbody|td|textarea|tfoot|th|thead|time|title|tr|track|tt|u|ul|var|video|wbr|xmp|\\?|\\%)([^>]*?)>"
 	s2 := "(?i)(<[^>]*)on[a-zA-Z]+\\s*=([^>]*>)"
 	s1Reg := regexp.MustCompile(s1)
 	s2Reg := regexp.MustCompile(s2)
--- validator/validator_test.go
+++ validator/validator_test.go
@@ -10,7 +10,13 @@ func TestXSS(t *testing.T) {
 	opt := ValidateOptions{
 		Value: "<Iframe srcdoc=<svg/o&#x6Eload&equals;alert&lpar;l)&gt;>",
 	}
+	opt1 := ValidateOptions{
+		Value: "<script>小小</script>",
+	}
 	fn := XSS()
+
 	fn(&opt)
+	fn(&opt1)
 	fmt.Println(opt)
+	fmt.Println(opt1)
 }
